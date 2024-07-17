5e7c9972ba4588021038230b56a9d87f793c02fa
jaewang1 <wodhkd2427@naver.com>
2024-07-17 17:16:25 +0900

    240717

---
+++ main.py
@@ -0,0 +1,82 @@
+#
+# for a in range(2,10) :
+#     for b in range(1,10):
+#         print(a,'x',b ,'=',a*b)
+
+#
+# def print_mul_table(X):
+#     for i in range(1,10) :
+#         print(X,'x',i,'=',X*i)
+#
+# print_mul_table(9)
+#
+# def dm(a=input("문자를 입력하세요:")) :
+#     if len(a) <= 20 :
+#         print("요금은 50원 입니다.")
+#         return 50
+#     elif len(a)>20 :
+#         print("요금은 100원 입니다")
+#         return 100
+# dm()
+#
+#
+# def score (x) :
+#     result=[]
+#     if 81<x<=100 :
+#         print("a 학점 입니다.")
+#         result='a'
+#     elif  61<x<=80 :
+#         print("b학점 입니다.")
+#         result = 'b'
+#     elif 41 < x <= 60 :
+#         print("c학점 입니다.")
+#         result = 'c'
+#     elif 21 < x <= 40:
+#         print("d학점 입니다.")
+#         result = 'd'
+#     elif 0 < x <= 20:
+#         print("e학점 입니다.")
+#         result = 'e'
+#     return result
+#
+# text=int(input('점수를 입력해주세요.:'))
+# var=score(text)
+# print(var)
+
+#
+# def mean_list (x) :
+#     return x
+
+
+#
+#
+# var ={1,2,3,4}
+# result = 0
+# for i in x :
+#     result = result + i
+# avg =(result / len(x))
+# return avg
+#
+# # mean _list()
+# #
+#
+#   ppp(x):
+#     result=""
+#     if x%2==0 :
+#         print('짝수')
+#         result="짝수"
+#     else:
+#         print('홀수')
+#         result='홀수'
+#     return result
+# num=int(input('숫자를 입력하세요.'))
+# var=ppp(num)
+# print(var)
+
+
+number = [100,200,300]
+print(vat(number))
+
+def =vat(x) :
+    for a in x :
+        
